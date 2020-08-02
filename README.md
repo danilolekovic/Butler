48913eda8f77ab17b2d510f9212022ca54d7706b
Danilo Lekovic <danilo@lekovic.ca>
2020-08-02 13:03:08 -0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # Butler
-an AI stock trading bot
+
+An AI stock trading bot using WealthSimple's API.
+
+## How It Works
+
+The bot will only either be buying one security at a time or selling one security at a time. It does not doo anything synchronously. It buys and sells in an alternating fashion. The bot also uses static thresholds for deciding when it will buy or sell.
+
+Basically, the bot will loop through the following logic:
+
+**Buying:**
+- 1) If the price has dropped enough, place a BUY order and change next state to SELL.
+- 2) If the price hasn't dropped enough, check for a trend indication. If there is a trend, return to step 1. If there isn't a trend, do nothing.
+
+**Selling:**
+- 1) If the current price indicates a profit, place a SELL order and change next state to BUY.
+- 2) If the price isn't giving us a desired profit, look for a trend. If there is a trend, go to step 1. If there isn't, do nothing.
+
+## License & Disclaimer
+
+The code in this repository is licensed under the permissive MIT license:
+
+```
+MIT License
+
+Copyright (c) 2020 Danilo Lekovic
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
+
+Nothing contained in this repository should be considered to be advice. I am not qualified to provide any sort of investment advice or legal advice. Any decisions, risks, or investments made with using anything in this repository is your responsibility.
+
+You should be very cautious and careful. Bots can lose you a lot of money very quickly. Everything in this repository is for **EDUCATIONAL PURPOSES ONLY** and is not meant to be used in the real world.
---
+++ src/butler.py
@@ -0,0 +1,131 @@
+import requests
+import math
+
+class Butler():
+    def __init__(self, email, password):
+        self.login(email, password)
+        self.getBalances()
+        self.isNextBuy = True
+        self.UPWARD_TREND_THRESH = 2.25
+        self.DIP_THRESH = 2.25
+        self.PROFIT_THRESH = 1.25
+        self.STOP_LOSS_THRESH = -2.00
+        self.lastOpPrice = 100.00
+
+        # need to get current owned quantity
+        self.quantity = 0
+    
+    def login(self, email, password):
+        api = "https://trade-service.wealthsimple.com/auth/login"
+        content = {"Content-Type": "application/json"}
+        data = {"email": email, "password": password}
+        request = requests.post(url = api, json = data, headers = content)
+
+        if "\"error\":\"Not authorized\"" in request.text:
+            error("Authentication failed")
+        else:
+            headers = request.headers
+            self.accessToken = headers["X-Access-Token"]
+            self.refreshToken = headers["X-Refresh-Token"]
+            self.header = {"Content-Type": "application/json", "Authorization": self.accessToken}
+    
+    def attemptToMakeTrade(self):
+        currentPrice = self.getMarketPrice()
+        percentageDiff = (currentPrice - self.lastOpPrice) / self.lastOpPrice * 100
+
+        if self.isNextBuy:
+            attemptBuy(percentageDiff)
+        else:
+            attemptSell(percentageDiff)
+    
+    def attemptBuy(self, percentageDiff):
+        if percentageDiff >= self.UPWARD_TREND_THRESH or percentageDiff <= self.DIP_THRESH:
+            self.lastOpPrice = placeBuyOrder()
+            self.isNextBuy = False
+    
+    def attemptSell(self, percentageDiff):
+        if percentageDiff >= UPWARD_TREND_THRESH OR percentageDiff <= self.DIP_THRESH:
+            self.lastOpPrice = placeSellOrder()
+            self.isNextBuy = True
+
+    # sends a GET request to the API for the account's balances
+    def getBalances(self):
+        api = "https://trade-service.wealthsimple.com/account/list"
+        request = requests.get(url = api, headers = self.header)
+        jsonResponse = request.json()
+        self.balance = float(jsonResponse.items()["current_balance"])
+    
+    # sends a GET request to the API for the current price of what we're trading
+    def getMarketPrice(self):
+        # some ticker here..
+        api = "https://trade-service.wealthsimple.com/securities?query="
+    
+    # 1) calculate the amount to sell (based on some threshold)
+    #     i.e., 50% of total balance
+    #     i.e., send a POST request to the API to do a sell operation
+    def placeBuyOrder(self):
+        api = "https://trade-service.wealthsimple.com/orders"
+
+        # figure out how to get price of security
+        price = 5.00
+        percent = 0.5
+
+        quantity = math.floor(math.floor(self.balance * percent)  / price)
+
+        # figure out how to get security ID, this is $APPL
+        securityID = "sec-s-76a7155242e8477880cbb43269235cb6"
+
+        data = {
+            "security_id": securityID,
+            "limit_price": price,
+            "quantity": quantity,
+            "order_type": "buy_quantity",
+            "order_sub_type": "limit",
+            "time_in_force": "day"
+        }
+
+        request = requests.post(url = api, json = data, headers = self.header)
+
+        filledAt = request.json().items()["filled_at"]
+        self.getBalances()
+
+        self.quantity = self.quantity + quantity
+        
+        # returns price at operation execution
+        filledAt
+    
+    # send a POST request to the API to do a sell operation
+    def placeSellOrder(self):
+        api = "https://trade-service.wealthsimple.com/orders"
+
+        # figure out how to get price of security
+        price = 5.00
+
+        # figure out how to get security ID, this is $APPL
+        securityID = "sec-s-76a7155242e8477880cbb43269235cb6"
+
+        data = {
+            "security_id": securityID,
+            "limit_price": price,
+            "quantity": self.quantity, # just sell everything
+            "order_type": "sell_quantity",
+            "order_sub_type": "limit",
+            "time_in_force": "day"
+        }
+
+        request = requests.post(url = api, json = data, headers = self.header)
+
+        filledAt = request.json().items()["filled_at"]
+        self.getBalances()
+        
+        # return price at operation execution
+        filledAt
+    
+    # logger to show the bot's actions
+    def log(self, type, message):
+        print("[", type, "]", message)
+    
+    # throws errors and quits
+    def error(self, message)
+        self.log("ERROR", message)
+        quit()
